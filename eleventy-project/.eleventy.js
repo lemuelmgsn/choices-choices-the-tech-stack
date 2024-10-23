afc35d0f0608c1212e60b4e50a460dc487f6266c
lemuelmgsn <magsinolemuel68@gmail.com>
2024-10-23 19:37:53 +0200

    added images and svg

--- eleventy-project/.eleventy.js
+++ eleventy-project/.eleventy.js
@@ -1,11 +1,16 @@
-module.exports = function 
-(eleventyConfig) {
+module.exports = function (eleventyConfig) {
     eleventyConfig.addPassthroughCopy("./src/css");
-    return{
+    eleventyConfig.addPassthroughCopy("./src/static");
+    
+    // Voeg watch targets toe om herbouw te triggeren bij wijzigingen
+    eleventyConfig.addWatchTarget("./src/css/");
+    eleventyConfig.addWatchTarget("./src/static/");
+    
+    // Return statement komt aan het einde
+    return {
         dir: {
             input: "src",
             output: "public",
         },
     };
-    eleventyConfig.addWatchTarget("./src/css/");
 };
